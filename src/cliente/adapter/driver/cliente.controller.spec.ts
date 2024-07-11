7e1cd9f4dc76c255aaf76824121870bd6fe83884
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-07-11 18:13:06 -0300

    feat: Criado teste simples para validação CI e CD

---
+++ src/cliente/adapter/driver/cliente.controller.spec.ts
@@ -0,0 +1,8 @@
+describe('ClienteController', () => {
+  describe('findAll', () => {
+    it('should test', async () => {
+      const result = ['test'];
+      expect(result).toEqual(result);
+    });
+  });
+});
