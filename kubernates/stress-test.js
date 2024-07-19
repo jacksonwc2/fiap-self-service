269a4fcc7d7b51326bcad053b6d1ede852f7297d
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-07-18 21:12:55 -0300

    feat: ajustes kinds

--- kubernates/stress-test.js
+++ kubernates/stress-test.js
@@ -1,13 +1,13 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
-  vus: 100000,
-  duration: '1m',
+  vus: 10,
+  duration: '5m',
 };
 
 // k6 run stress-test.js
 
 export default function () {
-  http.get('http://192.168.49.2:30000/produtos/{categoria}?categoria=LANCHE');
-  sleep(0.1);
+  http.get('http://localhost:30000/produtos/{categoria}?categoria=LANCHE');
+  sleep(1);
 }
