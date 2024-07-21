1244e65bd62b4732696f05222db1bda8f82e10f8
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-07-21 11:35:36 -0300

    feat: ajustes regras entidade cliente

--- src/core/cliente/entities/cliente.ts
+++ src/core/cliente/entities/cliente.ts
@@ -1,23 +1,40 @@
-import { IsEmail, IsNotEmpty, Length } from "class-validator";
-import { ValidarCPF } from "src/infrastructure/decorators/validarCPF.decorator";
+import { HttpException, HttpStatus } from "@nestjs/common";
 
 export class Cliente {
   id: string | null;
 
-  @IsNotEmpty()
-  @Length(3, 250)
   nome: string;
 
-  @IsEmail()
-  @IsNotEmpty()
-  @Length(0, 250)
   email: string;
 
-  @ValidarCPF()
-  @IsNotEmpty()
   cpf: string;
 
   constructor(nome: string, email: string, cpf: string) {
+    if (nome == null || nome.length > 250 || nome.length < 3) {
+      throw new HttpException("Nome inválido",  HttpStatus.BAD_REQUEST);
+    }
+
+    if (
+      email == null ||
+      email.length > 250 ||
+      email.length < 3 ||
+      !new RegExp("[A-Za-z0-9\\._-]+@[A-Za-z0-9]+\\..(\\.[A-Za-z]+)*").test(
+        email
+      )
+    ) {
+      throw new HttpException("e-mail inválido",  HttpStatus.BAD_REQUEST);
+    }
+
+    if (
+      cpf == null ||
+      cpf.length != 11 ||
+      !new RegExp("^[0-9]*$").test(
+        cpf
+      )
+    ) {
+      throw new HttpException("CPF inválido",  HttpStatus.BAD_REQUEST);
+    }
+
     this.nome = nome;
     this.email = email;
     this.cpf = cpf;
