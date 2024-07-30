f50e7a2db7a71d29688135de3a81a8cdd44faa38
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-07-29 22:28:57 -0300

    fix: ajustes configs kubectl

--- kubernetes/stress-test.js
+++ kubernetes/stress-test.js
@@ -1,13 +1,13 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
-  vus: 10,
-  duration: '5m',
+  vus: 100,
+  duration: '2m',
 };
 
 // k6 run stress-test.js
 
 export default function () {
   http.get('http://localhost:30000/produtos/{categoria}?categoria=LANCHE');
-  sleep(1);
+  sleep(0.1);
 }
